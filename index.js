78be866c8be4d6ff6a936cf5a0d55216602ce41e
ana <anamifischer@gmail.com>
2026-09-11 08:49:22 -0300

    sei la

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const server = express();
 
+const PORT = 3000;
+
 server.get('/produtos', (request, response) => {
     return response.send('Hello World!')
 });
 
-server.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+server.get('/produtos/:id', (req, res) =>{
+    const id = req.params.id;
+    return res.send(`O produto tem id = ${id}`);
+})
+
+server.post('/produtos', (request, response) => {
+    return res.send('Produto cadastrado com sucesso');
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 });
